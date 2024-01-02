5657e49cf886504b4da217a85caabaa5800df244
Kaustubh <fundekaustubh@gmail.com>
2024-01-02 17:06:05 +0530

    Basic server setup done.

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world!")
+	}
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is listening on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
